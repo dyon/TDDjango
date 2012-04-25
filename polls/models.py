db9d2450aa1ca39b1e89c53851d8a23c41889aeb
Dyon <admin@skwow.net>
2012-04-25 19:56:41 +0200

    Update the model adding the verbose name. test_verbose_name_for_pub_date() passes.

--- polls/models.py
+++ polls/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(verbose_name='Publish date')
