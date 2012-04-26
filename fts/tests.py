97c9515c50b7b8332de2be192cccb37c2aa90092
Dyon <admin@skwow.net>
2012-04-26 19:31:31 +0200

    We test the addition of choices to the poll in the functional test.

--- fts/tests.py
+++ fts/tests.py
@@ -61,6 +61,14 @@ class PollTest(LiveServerTestCase):
         time_field = self.browser.find_element_by_name('pub_date_1')
         time_field.send_keys('10:30')
 
+        # She enters choices for the poll
+        choice_1 = self.browser.find_element_by_name('choice_set-0-choice')
+        choice_1.send_keys('Very awesome')
+        choice_2 = self.browser.find_element_by_name('choice_set-1-choice')
+        choice_2.send_keys('Quite awesome')
+        choice_3 = self.browser.find_element_by_name('choice_set-2-choice')
+        choice_3.send_keys('Moderately awesome')
+
         # AJ clicks the save button
         save_button = self.browser.find_element_by_css_selector("input[value='Save']")
         save_button.click()
