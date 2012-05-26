44bae8dd1ff81b7538cf3789a2ffaa4267bb4067
Dyon <admin@skwow.net>
2012-05-26 15:53:40 +0200

    We create a dummy view.

--- TDDjango/urls.py
+++ TDDjango/urls.py
@@ -13,5 +13,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^$', 'polls.views.home'),  # Same as: url(r'^$', home)
     url(r'^admin/', include(admin.site.urls)),
 )
